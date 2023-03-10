6d061e3115d64ee9a4d2b16514b517c238f03ce0
chunkybooboo <89316044+chunkybooboo@users.noreply.github.com>
2023-03-10 00:06:57 -0800

    fixed style

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ return `<!DOCTYPE html>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" 
     crossorigin="anonymous">
+    <link rel="stylesheet" href="../src/style.css" />
     <title>Team Profile Generator</title>
 </head>
 <body>
+    <header>MY TEAM</header>
     <div id="employee-container" class="row">
         ${team.map(employee => employee.getHTML())}
     </div>
